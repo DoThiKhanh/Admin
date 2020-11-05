b72bf15176d189aacc189089fb7cfecef53bb8ac
DoThiKhanh <khanhdo22091999@gmail.com>
2020-11-05 13:49:51 +0700

    Update

--- src/app/main/product/product.module.ts
+++ src/app/main/product/product.module.ts
@@ -1,33 +1,45 @@
+
+import { BrandComponent } from './brand/brand.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { OrderComponent } from './order/order.component';
 import { ProductComponent } from './product/product.component';
 import { TypeComponent } from './type/type.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FileUploadModule } from 'primeng/fileupload';
+import { HttpClientModule} from '@angular/common/http';
 import {EditorModule} from 'primeng/editor';
+
+const routes: Routes = [
+  {
+    path: 'product',
+    component: ProductComponent
+  },
+  {
+    path: 'category',
+    component: TypeComponent
+  },
+  {
+    path: 'brand',
+    component: BrandComponent
+  },
+  { path: 'order', component: OrderComponent},
+];
+
 @NgModule({
   declarations: [
-    OrderComponent,ProductComponent,TypeComponent
+    OrderComponent,ProductComponent,TypeComponent,BrandComponent
   ],
   imports: [
     CommonModule,
+    RouterModule.forChild(routes),
     SharedModule,
-    EditorModule,
-    RouterModule.forChild([
-      {
-        path: 'order',
-        component: OrderComponent,
-      },
-      {
-        path: 'product',
-        component: ProductComponent,
-      },
-      {
-        path: 'type',
-        component: TypeComponent,
-      },
-  ]),
-  ]
+    ReactiveFormsModule,
+    FileUploadModule,
+    SharedModule,
+    HttpClientModule,
+    EditorModule  ]
 })
 export class ProductModule { }
